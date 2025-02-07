83e1721413d6881e2eed912219b3b3c82506df4c
warrenposo <93319382+warrenposo@users.noreply.github.com>
2025-02-07 16:09:40 +0300

    updates

---
+++ src/pages/CarTrackerDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
