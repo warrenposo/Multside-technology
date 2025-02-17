3c47e08ddec421393952f17ca4fdca65b0043491
warrenposo <93319382+warrenposo@users.noreply.github.com>
2025-02-17 10:25:11 +0300

    video banner optimization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import styles from './home.module.css';
 import backgroundVideo from '../Assets/backgroundvid.mp4'; // Import the video
 
 const Home = () => {
   return (
     <div className={styles.banner}>
+      {/* Manage the head section */}
+      <Helmet>
+        <link
+          rel="preload"
+          href="https://multside.co.ke/assets/animation.gif"
+          as="image"
+          type="image/gif"
+        />
+      </Helmet>
+
       {/* Video Background */}
       <video autoPlay loop muted className={styles.videoBackground}>
         <source src={backgroundVideo} type="video/mp4" />
@@ -19,6 +30,14 @@ const Home = () => {
         <div className={styles.textContent}>
           <h1 className={styles.gradientText}>MULT-SIDE TECHNOLOGIES</h1>
           <p className={styles.tagline}>Keeping You Safe and Secure</p>
+
+          {/* Preloaded Image */}
+          {/* <img
+            src="https://multside.co.ke/assets/animation.gif"
+            alt="Animated GIF"
+            width="300"
+            height="200"
+          /> */}
         </div>
       </div>
     </div>
